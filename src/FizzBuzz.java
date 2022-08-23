8cf76db9bdcca78d64c036677d861760c368652e
0x399 <pryimak.sviat@gmail.com>
2022-08-23 20:53:59 +0300

    Balls

---
+++ src/FizzBuzz.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+    public static List<String> fizzBuzz(int n) {
+        ArrayList<String> result = new ArrayList<String>();
+        for (int i = 1; i <= n; i++){
+            if ((i%5)==0 && (i%3)==0){
+                result.add("FizzBuzz");
+            }
+            else if ((i%5)==0){
+                result.add("Buzz");
+            }
+            else if ((i%3)==0){
+                result.add("Fizz");
+            }
+            else{
+                String s=String.valueOf(i);
+                result.add(s);
+            }
+        }
+        System.out.println(result);
+        System.out.println(result.hashCode());
+        return result;
+    }
+    public static void main(String... args){
+        FizzBuzz.fizzBuzz(15);
+    }
+}
